532880f031db12744db98e6d424dbfbac1f9028c
JCEntrena <jentrena@correo.ugr.es>
2015-03-16 20:50:39 +0100

    Añadida exclusión mutua.

--- P1/lectorescritor.cpp
+++ P1/lectorescritor.cpp
@@ -13,17 +13,28 @@ const unsigned tam_cola = 10;
 queue<int> cola; 
 static int generados = 0;  
 
-sem_t puede_leer, puede_escribir; 
+sem_t puede_leer, puede_escribir, e_mutua; 
 
 int producir_dato(){
     return generados++;
 }
 
+/**
+ * Función que escribe un dato entero en la cola.
+ */
 void escribe(int dato){
+    // Inicio de la exclusión mutua para acceder a la cola
+    sem_wait(&e_mutua); 
     cola.push(dato); 
+    sem_post(&e_mutua); 
+    
     sem_post(&puede_leer);
 }
 
+/**
+ * Función que irá produciendo datos y llamará a escribe(int) para
+ * escribirlos en la cola. Le asignaremos una hebra. 
+ */
 void * productor(void *){
 
     for(unsigned i = 0 ; i < num_items ; i++){
@@ -34,16 +45,26 @@ void * productor(void *){
     return NULL;
 }
 
+/** 
+ * Imprime el entero dato por pantalla. 
+ */ 
 void consumir_dato(int dato){ 
     cout << "He leído un dato: " << dato << endl; 
 }
 
+/**
+ * Función que irá leyendo datos de la cola. Le asignaremos una hebra. 
+ */
 void * consumidor(void *){
     for(unsigned i = 0 ; i < num_items ; i++){
         int dato;
-        sem_wait(&puede_leer); 
-        dato = cola.front();            // Falta añadir exclusión mutua.
+        sem_wait(&puede_leer);          // Esperamos a poder leer. 
+        // Exclusión mutua
+        sem_wait(&e_mutua); 
+        dato = cola.front();            
         cola.pop();  
+        sem_post(&e_mutua); 
+
         sem_post(&puede_escribir);
         consumir_dato(dato);    
     }
@@ -55,6 +76,8 @@ int main(){
 
     sem_init(&puede_leer, 0, 0); 
     sem_init(&puede_escribir, 0, tam_cola); 
+    sem_init(&e_mutua, 0, 1); 
+
     pthread_t hebral, hebrae;  
 
     pthread_create(&hebrae, NULL, productor, NULL);
